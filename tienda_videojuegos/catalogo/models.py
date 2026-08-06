473ce798e6e8885cb6bd96446e7472e926cbbaa6
FranRoy07 <franchoroy7gmail.com>
2026-08-06 17:50:55 -0300

    feat: creación modelo en catalogo (Juego) y migración de datos de la view catalogo a SQLite

--- tienda_videojuegos/catalogo/models.py
+++ tienda_videojuegos/catalogo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Juego(models.Model): #Herencia de modelos para que funcione bien django internamente
+    nombre = models.CharField(max_length=100) #charfield sería como el varchar en sql
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    plataforma = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.nombre
+    
+    
+    
+    
+    
