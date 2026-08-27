47e102664665c38ef4a78dffd994f5913172c964
FranRoy07 <franchoroy7gmail.com>
2026-08-27 17:39:36 -0300

    feat: reajuste en la sección favoritos y el corazón.

--- tienda_videojuegos/carrito/context_processors.py
+++ tienda_videojuegos/carrito/context_processors.py
@@ -1,10 +1,16 @@
 from .models import Carrito
+from catalogo.models import Favorito
 
 def carrito_total(request):
     total_items = 0
+    total_favoritos = 0
     if request.user.is_authenticated:
         carrito = Carrito.objects.filter(usuario=request.user).first()
         if carrito:
             total_items = carrito.total_items()
-    return {'carrito_total_items': total_items}
+        total_favoritos = Favorito.objects.filter(usuario=request.user).count()
+    return {
+        'carrito_total_items': total_items,
+        'favoritos_total': total_favoritos,
+    }
 
