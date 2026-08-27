571da6e3a993bc53dffb6f9c276c6ab39cb4fb08
FranRoy07 <franchoroy7gmail.com>
2026-08-27 13:35:04 -0300

    feat: Implementación de notificaciones tipo Toast

--- tienda_videojuegos/static/js/script.js
+++ tienda_videojuegos/static/js/script.js
@@ -53,3 +53,15 @@ function togglePassword(fieldId){
         icon.classList.add('fa-eye');
     }
 }
+
+// Mensajes flotantes
+document.addEventListener("DOMContentLoaded", function () {
+    const toastElList = [].slice.call(document.querySelectorAll('.toast'));
+    toastElList.forEach(function (toastEl) {
+        const toast = new bootstrap.Toast(toastEl, {
+            delay: 3000
+        });
+        toast.show();
+    });
+});
+
--- tienda_videojuegos/usuarios/views.py
+++ tienda_videojuegos/usuarios/views.py
@@ -1,45 +1,40 @@
 from django.shortcuts import render, redirect
-
-#Librerías importadas
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import RegistroForm, LoginForm
 from django.contrib import messages
 
-# Create your views here.
 def registro_view(request):
     if request.method == 'POST':
         form = RegistroForm(request.POST)
-        if form.is_valid(): #verifica que se cumplan las condiciones para registrarse correctamente.
+        if form.is_valid():
             usuario = form.save()
-            login(request, usuario) #automáticamente inicia sesión si se pudo registrar (POST) el usuario
+            login(request, usuario)
+            messages.success(request, "Registro completado correctamente.")
             return redirect('home')
-    else: #el formulario cuando se apreta en registrarse tiene que estar vacío lógicamente (GET)
+    else:
         form = RegistroForm()
     return render(request, 'usuarios/registro.html', {'form': form})
-
-#POST = enviar datos
 
 def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request, data=request.POST)
-        if form.is_valid(): #Comprueba credenciales, el usuario debe coincidir con el que está registrado en bdd.
+        if form.is_valid():
             usuario = form.get_user()
             login(request, usuario)
+            messages.success(request, "Inicio de sesión correcto.")
             return redirect('home')
         else:
-            #mostrar errores generales en consola para debug
-            print(form.errors)
             messages.error(request, "Usuario o contraseña incorrectos.")
-    else: #GET, no se apretó en 'Ingresar'
-        form = LoginForm() #mostrar el login vacío al apretar en loguearse.
+    else:
+        form = LoginForm()
     return render(request, 'usuarios/login.html', {'form': form})
 
 def logout_view(request):
-    logout(request) #lo hace internamente Django
+    logout(request)
+    messages.info(request, "Has cerrado sesión correctamente.")
     return redirect('login')
 
-@login_required #Se activa si se logueó correctamente el usuario, si no se cumple Django lo envía a la página de login
+@login_required
 def perfil_view(request):
     return render(request, 'usuarios/perfil.html')
-
